a13ff7985e2d19d54866defd87ca317a0d0c04f0
Yash <yh7348@github.com>
2025-12-08 01:34:24 +0530

    fix: update base path to correct repository name zebra-resource-planning-app

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/zebra-resource-planner/" : "/",
+  base: process.env.NODE_ENV === "production" ? "/zebra-resource-planning-app/" : "/",
   server: {
     host: "::",
     port: 8080,
